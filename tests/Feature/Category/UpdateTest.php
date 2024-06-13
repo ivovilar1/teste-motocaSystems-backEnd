758b485eefe9a77170a8d096db185c0eb9f69aad
Ivo Vilar <ivo@grupovoz.dev.br>
2024-06-13 15:08:28 -0300

    validation rules update category

--- tests/Feature/Category/UpdateTest.php
+++ tests/Feature/Category/UpdateTest.php
@@ -4,13 +4,51 @@ use App\Models\Category;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\putJson;
 
+beforeEach(function () {
+   $this->category = Category::factory()->create();
+});
+
 it('should be able to update a category', function () {
-    $category = Category::factory()->create();
-    putJson(route('category.update', $category), [
+    putJson(route('category.update', $this->category), [
         'name' => 'Category updated',
     ])->assertOk();
     assertDatabaseHas('categories', [
-        'id' => $category->id,
+        'id' => $this->category->id,
         'name' => 'Category updated',
     ]);
+});
+
+describe('validation rules', function () {
+
+    test('category.required', function () {
+        putJson(route('category.update', $this->category),[])
+            ->assertJsonValidationErrors([
+                'name' => __('validation.required', ['attribute' => 'name'])
+            ]);
+    });
+
+    test('category.min', function () {
+        putJson(route('category.update', $this->category),[
+            'name' => 'ab'
+        ])
+            ->assertJsonValidationErrors([
+                'name' => __('validation.min.string', ['attribute' => 'name', 'min' => 3])
+            ]);
+    });
+    test('category.max', function () {
+        putJson(route('category.update', $this->category),[
+            'name' => str_repeat('a', 51)
+        ])
+            ->assertJsonValidationErrors([
+                'name' => __('validation.max.string', ['attribute' => 'name', 'max' => 50])
+            ]);
+    });
+    test('category.string', function () {
+        putJson(route('category.update', $this->category),[
+            'name' => 1321231231
+        ])
+            ->assertJsonValidationErrors([
+                'name' => __('validation.string', ['attribute' => 'name'])
+            ]);
+    });
 });
